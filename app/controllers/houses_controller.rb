57fc68ef7f2fdfa8e949964430d2a602d9e3b8b4
dilan <dilanrincon@gmail.com>
2024-05-27 18:05:56 -0700

    Creating C on my CRUD

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -6,4 +6,23 @@ class HousesController < ApplicationController
   def show
     @house = House.find(params[:id])
   end
+
+  def new
+    @house = House.new
+  end
+
+  def create
+    @house = House.new(title: "...", image: "...", description: "...", price: "...")
+
+    if @house.save
+      redirect_to @house
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def house_params
+      params.require(:house).permit(:title, :image, :description, :price)
+    end
 end
